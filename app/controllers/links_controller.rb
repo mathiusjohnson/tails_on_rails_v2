a73b9c6a7186d4ca3771aa2a501e0905793690f1
Mathius <devachievem@gmail.com>
2021-08-12 15:07:22 -0700

    add create route

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -19,13 +19,39 @@ class LinksController < ApplicationController
         }
       }
     GQL
-    
-    response = ProjectsSchema.execute(query: query_string)
+    context = {
+      current_user: current_user
+    }
+    response = ProjectsSchema.execute(query: query_string, variables: nil, context: context)
     @data = response.to_h["data"]["allLinks"]
   end
 
   def show
     @link = Links.find(params[:id])
+  end
+
+  def create
+    
+    create_link_query_string = <<~GQL
+      mutation {
+        createLink(
+          url: "https://www.link.ca",
+          description: "Best tools leo!",
+        ) {
+          id
+          url
+          description
+          postedBy {
+            id
+            email
+          }
+        }
+      }
+    GQL
+
+    user = current_user
+    response = ProjectsSchema.execute(query: create_link_query_string)
+
   end
 
 end
