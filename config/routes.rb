b3dd50ea4fc25632f6aaaf0957e9431f60cec605
Mathius <devachievem@gmail.com>
2021-08-13 14:53:00 -0700

    add new next and previous links routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users
 
   if Rails.env.development?
     authenticate :user do
       mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
     end
   end
+  
   post "/graphql", to: "graphql#execute"
   
   get 'welcome/index'
   get "/links" => "links#index"
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  get 'new_link', to: 'links#new'
+  get "previous_page", to: "links#previous_page"
+  get "next_page", to: "links#next_page"
   post 'create_link', to: 'links#create'
+
   root to: "welcome#index"
 
 end
