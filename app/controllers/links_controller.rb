3a058ec2868055c69de5482327c28ecd02d784bb
Mathius <devachievem@gmail.com>
2021-08-13 14:51:18 -0700

    add next and previous page get links requests

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,42 +1,62 @@
 require 'json'
 
-class LinksController < ApplicationController
+class LinksController < GraphqlController
+
+  def executeFromChild(args)
+    query = args[:query]
+    variables = args[:variables]
+    context = args[:context]
+    super(query, variables, context)
+  end
   
   def index
     query_string = <<~GQL
       query {
-        allLinks {
-          id
-          description
-          postedBy {
-            email
+        linksPaginated {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
           }
-          votes {
-            user {
-              email
+          edges {
+            cursor
+            node {
+              id
+              url
+              description
+              postedBy {
+                email
+              }
+              votes {
+                id
+              }
             }
           }
         }
       }
     GQL
-    context = {
-      current_user: current_user
-    }
-    response = ProjectsSchema.execute(query: query_string, variables: nil, context: context)
-    @data = response.to_h["data"]["allLinks"]
+
+    response = executeFromChild(query: query_string, variables: nil, context: context)
+    @data = JSON.parse(response)["data"]["linksPaginated"]
+
   end
 
   def show
     @link = Links.find(params[:id])
   end
 
+  def new
+    respond_to do |format|
+        format.js { render partial: 'links/form' }
+    end
+  end
+
   def create
-    
+    url, description = new_link_params.values_at(:url, :description)
     create_link_query_string = <<~GQL
       mutation {
         createLink(
-          url: "https://www.link.ca",
-          description: "Best tools leo!",
+          url: "#{url}",
+          description: "#{description}",
         ) {
           id
           url
@@ -49,9 +69,80 @@ class LinksController < ApplicationController
       }
     GQL
 
-    user = current_user
-    response = ProjectsSchema.execute(query: create_link_query_string)
+    response = executeFromChild(query: create_link_query_string,  variables: nil, context: context)
+    
+    redirect_to links_path
+  end
 
+  def next_page
+    cursor = params["cursor"]
+
+    query_string = <<~GQL
+    query {
+      linksPaginated(after: "#{cursor}") {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+        }
+        edges {
+          cursor
+          node {
+            id
+            url
+            description
+            postedBy {
+              email
+            }
+            votes {
+              id
+            }
+          }
+        }
+      }
+    }
+  GQL
+
+  response = executeFromChild(query: query_string, variables: nil, context: context)
+  @data = JSON.parse(response)["data"]["linksPaginated"]
+  render 'links/index'
+  end
+
+  def previous_page
+    cursor = params["cursor"]
+    query_string = <<~GQL
+      query {
+        linksPaginated(before: "#{cursor}") {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+          }
+          edges {
+            cursor
+            node {
+              id
+              url
+              description
+              postedBy {
+                email
+              }
+              votes {
+                id
+              }
+            }
+          }
+        }
+      }
+    GQL
+
+    response = executeFromChild(query: query_string, variables: nil, context: context)
+    @data = JSON.parse(response)["data"]["linksPaginated"]
+    render 'links/index'
+
+  end
+
+  private
+  def new_link_params
+    params.require(:link).permit(:url, :description)
   end
 
 end
